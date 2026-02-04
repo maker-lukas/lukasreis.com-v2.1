98ae02f199deab2a94fb31f0650840f3e5134e88
Lukas <itslukasreis@gmail.com>
2026-02-04 16:38:51 +0000

    add print.lukasreis.com to projects

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -6,6 +6,12 @@ export const projects = [
     github: "https://github.com/maker-lukas/40-ish-split-keyboard",
   },
   {
+    title: "print.lukasreis.com",
+    description: "A website where you can leave a small message and it will auto-print on my home printer!",
+    image: "/images/print.png",
+    github: "https://print.lukasreis.com"
+  },
+  {
     title: "B.E.T.T",
     description: "Better Enchanted Tool Textures. A Minecraft Resouce pack that changes tool textures depending on the enchantments",
     image: "/images/pack.png",
